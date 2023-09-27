6570fe61ead10e71b4516c5c4eaa34250192cfd1
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-27 23:07:03 +0900

    htmlファイルに直接　utf8を書き込む方法

--- 06_backend/020_path_control/start/app.mjs
+++ 06_backend/020_path_control/start/app.mjs
@@ -11,9 +11,19 @@ import * as http from "http";
 
 const server = http.createServer(function(req, res) {
     console.log(req.url);
-    res.writeHead(200, {'content-Type':'text/html; charset=UTF-8'});
+    // res.writeHead(200, {'content-Type':'text/html; charset=UTF-8'});
     if (req.url === '/hello') {
-        res.end('<h1>こんにちは</h1>')
+        res.end(`<!DOCTYPE html>
+        <html lang="ja">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+        </head>
+        <body>
+          <h1>こんにちは</h1>
+        </body>
+        </html>`)
     } else if (req.url === '/bye') {
         res.end('bye');
     }
