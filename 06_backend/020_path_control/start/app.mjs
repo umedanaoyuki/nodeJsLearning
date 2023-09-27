52ecbd5b321e0bcd36e0dd71319f1f5828d1a0b8
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-27 22:43:11 +0900

    アクセスするURLで　サーバーからのレスポンスを変更する

--- 06_backend/020_path_control/start/app.mjs
+++ 06_backend/020_path_control/start/app.mjs
@@ -1,8 +1,13 @@
 import * as http from "http";
 
+// http://localhopst:8080/
 const server = http.createServer(function(req, res) {
     console.log(req.url);
-    res.end('bye');
+    if (req.url === '/hello') {
+        res.end('hello')
+    } else if (req.url === '/bye') {
+        res.end('bye');
+    }
 });
 
 server.listen(8080);
