6e860dc3de5ce6630f3e126cf67a51af69a75252
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-27 23:03:57 +0900

    content type追加

--- 06_backend/020_path_control/start/app.mjs
+++ 06_backend/020_path_control/start/app.mjs
@@ -1,10 +1,19 @@
 import * as http from "http";
 
 // http://localhopst:8080/
+
+//content-type:
+// ブラウザとサーバーが通信しているコンテンツのフォーマットと文字コードの情報
+
+//text/html; charset=UTF-8
+
+
+
 const server = http.createServer(function(req, res) {
     console.log(req.url);
+    res.writeHead(200, {'content-Type':'text/html; charset=UTF-8'});
     if (req.url === '/hello') {
-        res.end('<script>window.alert("frontEnd")</script>')
+        res.end('<h1>こんにちは</h1>')
     } else if (req.url === '/bye') {
         res.end('bye');
     }
