0267134926d1f705ed2e293f8eef4303db49d3ed
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-27 22:49:57 +0900

    フロントエンド側であれば window.alertが機能する

--- 06_backend/020_path_control/start/app.mjs
+++ 06_backend/020_path_control/start/app.mjs
@@ -4,7 +4,7 @@ import * as http from "http";
 const server = http.createServer(function(req, res) {
     console.log(req.url);
     if (req.url === '/hello') {
-        res.end('<script>console.log("frontEnd")</script>')
+        res.end('<script>window.alert("frontEnd")</script>')
     } else if (req.url === '/bye') {
         res.end('bye');
     }
