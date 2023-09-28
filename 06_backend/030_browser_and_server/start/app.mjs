754a47700ddea2811571abeb29d64a5c83032a94
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-28 22:59:55 +0900

    HTTPメソッド２

--- 06_backend/030_browser_and_server/start/app.mjs
+++ 06_backend/030_browser_and_server/start/app.mjs
@@ -2,7 +2,7 @@ import * as http from "http";
 
 const server = http.createServer(function(req, res) {
     if(req.url === '/') {
-        res.write(`<a href="/result?param1=パラメータ1&param2=パラメータ2">Get Method Link</a>`);
+        res.write(`<a href="/result?param1=1&param2=2">Get Method Link</a>`);
         res.end(`
         <form action="/result" method="POST">
             <input type="text" name="title">
@@ -14,9 +14,13 @@ const server = http.createServer(function(req, res) {
         console.log(req.method);
         if (req.method === "GET") {
             //GETのパラメータを取得
-            new URLSearchParams(req.url);
+            const queryString = req.url.split("?")[1];
+            const params = new URLSearchParams(queryString);
+            console.log("ぱらむすs");
+            console.log(params);    
+        }
         res.end(req.url);
-    }}
-});
+    }
+    });
 
 server.listen(8080);
