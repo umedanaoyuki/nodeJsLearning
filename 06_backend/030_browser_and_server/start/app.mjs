a99f374f25b4e77862120d42696ac061c7858821
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-28 23:21:26 +0900

    express入門

--- 06_backend/030_browser_and_server/start/app.mjs
+++ 06_backend/030_browser_and_server/start/app.mjs
@@ -1,35 +1,55 @@
 import * as http from "http";
+import express from "express";
 
-const server = http.createServer(function(req, res) {
-    if(req.url === '/') {
-        res.write(`<a href="/result?param1=1&param2=2">Get Method Link</a>`);
-        res.end(`
+const PORT = 8080;
+const app = express();
+
+
+app.get("/", function(req,res) {
+        res.send(`
+        <a href="/result?param1=1&param2=2">Get Method Link</a>
         <form action="/result" method="POST">
             <input type="text" name="title">
             <input type="submit">
         </form>
         `)
-    } else {
-        console.log(req.url);
-        console.log(req.method);
-        if (req.method === "GET") {
-            //GETのパラメータを取得
-            const queryString = req.url.split("?")[1];
-            const params = new URLSearchParams(queryString);
-            console.log("ぱらむすs");
-            console.log(params);    
-        } else if (req.method === "POST") {
-            let data = "";
-            req.on("data", function (chunk) {
-                data += chunk;
-            });
-            req.on("end", function() {
-                const params = new URLSearchParams(data);
-                console.log(params);
-            })
-        }
-        res.end(req.url);
-    } 
-});
+})
 
-server.listen(8080);
+app.listen(PORT, () => {
+    console.log(`Server Start: http://localhost:${PORT}`)
+})
+
+
+// const server = http.createServer(function(req, res) {
+//     if(req.url === '/') {
+//         res.write(`<a href="/result?param1=1&param2=2">Get Method Link</a>`);
+//         res.end(`
+//         <form action="/result" method="POST">
+//             <input type="text" name="title">
+//             <input type="submit">
+//         </form>
+//         `)
+//     } else {
+//         console.log(req.url);
+//         console.log(req.method);
+//         if (req.method === "GET") {
+//             //GETのパラメータを取得
+//             const queryString = req.url.split("?")[1];
+//             const params = new URLSearchParams(queryString);
+//             console.log("ぱらむすs");
+//             console.log(params);    
+//         } else if (req.method === "POST") {
+//             let data = "";
+//             req.on("data", function (chunk) {
+//                 data += chunk;
+//             });
+//             req.on("end", function() {
+//                 const params = new URLSearchParams(data);
+//                 console.log(params);
+//             })
+//         }
+//         res.end(req.url);
+//     } 
+// });
+
+// server.listen(8080);
